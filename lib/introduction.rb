aa39508cdf97b274e7c7c4a37375c2f61bbaa801
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-23 22:36:43 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning ot program in #{language}."
 end
